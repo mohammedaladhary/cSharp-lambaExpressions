bb1cacdf60e707e780d77d775aff54999ba739da
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-24 12:40:36 +0400

    coalesing operator and list operations using lambda

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambaExpressions
+{
+    internal class Person
+    {
+        // Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+
+        // Constructor
+        public Person(string Name, int Age, string Gender, string Address)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Gender = Gender;
+            this.Address = Address;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace lambaExpressions
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(abc));
+            //Thread thread = new Thread(new ThreadStart(abc));
             //Func<int, int> square = (x) => x * x;
 
             //Func<int, int> square = (x) =>
@@ -15,17 +15,45 @@ namespace lambaExpressions
             //    return x * x;
             //};
 
-            MathOperation mathOperation = Add;
+            //MathOperation mathOperation = Add;
 
-            int result = mathOperation(5, 3);
+            //int result = mathOperation(5, 3);
 
-            mathOperation = multiply;
+            //mathOperation = multiply;
 
-            mathOperation(10, 10);
+            //mathOperation(10, 10);
 
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
 
-            SomeClass someClass = new SomeClass(multiply);
+            //SomeClass someClass = new SomeClass(multiply);
+
+            List<Person> list = new List<Person>()
+            {
+               new Person("salim", 22, "Male", "sohar"),
+            new Person("salim", 23, "Male", "sohar"),
+            new Person("salim", 25, "Male", "sohar"),
+            new Person("ali", 20, "Male", "muscat"),
+            new Person("shagufta", 25, "Female", "muscat"),
+            new Person("Aliya", 19, "Female", "sohar"),
+            new Person("Fariha", 30, "Female", "muscat"),
+            new Person("Farzana", 35, "Female", "salalah"),
+            new Person("Muqeet", 20, "Male", "sohar"),
+            new Person("Sehar", 18, "Female", "muscat")
+            };
+
+            List<Person> persons = list
+                .Where(x => x.Age <= 25)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+
+            Person? person = list.FirstOrDefault(x => x.Address == "muscat");
+            //if(person != null)
+            //{
+            //    Console.WriteLine(person.Name);
+            //}
+            Console.WriteLine(person?.Name ?? "Hello"); //coalesing operator
+            printPersonList(persons);
 
         }
 
@@ -35,7 +63,26 @@ namespace lambaExpressions
         static int multiply(int x, int y) => x * y;
         static void abc()
         {
-
+            //abc
+        }
+        public List<Person> getByAge (int age, List<Person> persons)
+        {
+            List <Person> agePersons = new List<Person>();
+            foreach (Person person in persons)
+            {
+                if(person.Age > age)
+                {
+                    agePersons.Add(person);
+                }
+            }
+            return agePersons;
+        }
+        static void printPersonList(List<Person>people)
+        {
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.Name);
+            }
         }
     }
 }
