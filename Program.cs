ef233423f70f57397c782b0dbd164fa4be3ff56b
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-24 11:13:03 +0400

    lambda expressions

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace lambaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(abc));
+            //Func<int, int> square = (x) => x * x;
+
+            //Func<int, int> square = (x) =>
+            //{
+            //    Console.WriteLine(x);
+            //    return x * x;
+            //};
+
+            MathOperation mathOperation = Add;
+
+            int result = mathOperation(5, 3);
+
+            mathOperation = multiply;
+
+            mathOperation(10, 10);
+
+            Console.WriteLine(result);
+
+            SomeClass someClass = new SomeClass(multiply);
+
+        }
+
+        public delegate int MathOperation(int x, int y);
+
+        static int Add (int x, int y) => x + y;
+        static int multiply(int x, int y) => x * y;
+        static void abc()
+        {
+
+        }
+    }
+}
---
+++ SomeClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static lambaExpressions.Program;
+
+namespace lambaExpressions
+{
+    internal class SomeClass
+    {
+        public SomeClass(MathOperation mathOperation)
+        {
+            int result = mathOperation(7, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
